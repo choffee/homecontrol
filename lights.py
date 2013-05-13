264e40671bbf77b7379cbb4c58a6c7d851cb3c76
John Cooper <john@choffee.co.uk>
2013-05-13 21:11:22 +0100

    Simple light control script

---
+++ lights.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Script to turn the lights on and off
+#
+# Expects on or off and a pram that's it.
+#
+# It then send the right codes to the Arduino
+#
+#
+import serial
+import sys
+
+if len(sys.argv) != 2:
+  print "Expects argument of on or off"
+  sys.exit(0)
+
+DEV='/dev/ttyUSB0'
+
+ser = serial.Serial(DEV, 9600, timeout=2)
+print ser.readline()
+
+if sys.argv[1] == 'on':
+  ser.write('RF  A2on\n')
+  print ser.readline()
+elif sys.argv[1] == 'off':
+  ser.write('RF  A2off\n')
+else:
+  print "Expects on or off as argument"
+
+ser.close()
+
+
+
+
