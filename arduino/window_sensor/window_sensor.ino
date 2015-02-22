dd24287ec2422489d6e7f56eb1e6d7d26dad4e50
John Cooper <john@choffee.co.uk>
2015-02-22 21:40:58 +0000

    Added SRF code

--- arduino/window_sensor/window_sensor.ino
+++ arduino/window_sensor/window_sensor.ino
@@ -2,7 +2,7 @@
 #include "DHT.h"
 
 
-#define RELAY_0_PIN D0
+#define RELAY_0_PIN 0
 #define DHT_PIN 5
 #define SOIL_POWER_PIN 7
 #define SOIL_0_PIN A0
@@ -11,9 +11,30 @@
 
 DHT dht(DHT_PIN, DHTTYPE);
 
+String inputString = "";
+boolean stringComplete = false;
+
 void setup() {
-  Serial.begin(9600);
+  // Turn on the SRF stuff
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  Serial.begin(115200);
+  Serial.print("+++");
+  delay(1000);
+  // Setup some things
+  //http://openmicros.org/index.php/articles/88-%C2%AD%E2%80%90ciseco-%C2%AD%E2%80%90product-%C2%AD%E2%80%90documentation/260-%C2%AD%E2%80%90srf-%C2%AD%E2%80%90configuration
+  // Set encryption key
+  Serial.println("ATEA HOMEAUTO");
+  // Turn on encryption
+  Serial.println("ATEE");
+  // Set the PAN ID
+  Serial.println("ATID 3489");
+  // Exit config mode
+  Serial.println("ATDN");
+  // Comment out above for default serial
+  // Serial.begin(9600);
   Serial.println("Window Sensor");
+  inputString.reserve(200);
   dht.begin();
   
   pinMode(RELAY_0_PIN, OUTPUT);
@@ -28,9 +49,31 @@ void setup() {
 int soilSensor = 0;
 
 void loop() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n' ) {
+      stringComplete = true;
+    }
+  }
+  if (stringComplete) {
+    if (inputString == "RELAY:0:ON\n") {
+      digitalWrite(RELAY_0_PIN, HIGH);
+      Serial.println("RELAY:0:STATE:ON");
+    }
+    if (inputString == "RELAY:0:OFF\n") {
+      digitalWrite(RELAY_0_PIN, LOW);
+      Serial.println("RELAY:0:STATE:OFF");
+    }
+    Serial.print("echo:");
+    Serial.println(inputString);
+    inputString = "";
+    stringComplete = false;
+    
+  }
   
   delay(2000);
-  digitalWrite(SOIL_POWER_PIN HIGH);
+  digitalWrite(SOIL_POWER_PIN, HIGH);
   delay(100);
   soilSensor = analogRead(SOIL_0_PIN);
   Serial.print("Soil Sensor 0: ");
@@ -38,7 +81,7 @@ void loop() {
   soilSensor = analogRead(SOIL_1_PIN);
   Serial.print("Soil Sensor 1: ");
   Serial.println(soilSensor);
-  
+  digitalWrite(SOIL_POWER_PIN, LOW);
   
   
   float h = dht.readHumidity();
@@ -50,11 +93,13 @@ void loop() {
   }
   
   Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
+  Serial.println(h);
   Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C ");
+  Serial.println(t);
+
   
   
 }
+
+
+
